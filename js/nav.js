8a24abd232a3d0bf1466e9779f5ad5312773e4e5
Anderson Araujo Macedo <andersonamacedo@hotmail.com>
2020-04-08 21:05:59 -0300

    v0.0.2

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,40 @@
-$.get("https://raw.githubusercontent.com/dojiDoMal/Desenvolvimento_WEB/master/HTML/navbar.template.html", function(data){
+$.get("https://raw.githubusercontent.com/dojiDoMal/DFMacedo/master/templates/navbar.template.html", function(data){
     $("#nav-placeholder").html(data);
-    var a = $("#cadastrarVendedor");
-    a.click(formVendedor);
+    var cadastraVendendor = $("#cadastrarVendedor");
+    var criaVenda = $("#venda");
+    var formVendedor = $("#formVendedor")
+    cadastraVendedor.click(formVendedor);
+    criaVenda.click(formVenda);
 });
 
-function formVendedor(){
-    $.get("https://raw.githubusercontent.com/dojiDoMal/Desenvolvimento_WEB/master/HTML/form.vendedor.html", function(data){
+function formVenda(){
+    $.get("https://raw.githubusercontent.com/dojiDoMal/DFMacedo/master/templates/form.venda.html", function(data){
         $("#template-placeholder").html(data);
     });
 }
+
+function formVendedor(){
+    $.get("https://raw.githubusercontent.com/dojiDoMal/DFMacedo/master/templates/form.vendedor.html", function(data){
+        $("#template-placeholder").html(data);
+    });
+}
+
+function registraFormVendedor(form){
+    $(document).ready(function() {
+        $("#btn").click(function(e){
+            var jsonData = {};
+            var formData = $(form).serializeArray();
+            $.each(formData, function() {
+                if (jsonData[this.name]) {
+                    if (!jsonData[this.name].push) {
+                    jsonData[this.name] = [jsonData[this.name]];
+                    }
+                jsonData[this.name].push(this.value || '');
+                } else {
+                    jsonData[this.name] = this.value || '';
+                }
+            })
+        })
+    });
+    console.log(jsonData); 
+} 
